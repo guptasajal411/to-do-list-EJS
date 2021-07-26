9129e6c568bec0fda292b5cd001829e607d2e0fc
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2021-07-26 16:53:13 +0530

    Add all days of the week for rendering by view engine

--- server.js
+++ server.js
@@ -11,12 +11,32 @@ app.get("/", function(req, res){
     var currentDay = today.getDay();
     var day = "";
 
-    if (currentDay == 6 || currentDay == 0){
-        day = "Weekend";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Satuday";
+            break;
+        default:
+            break;
     }
-    else{
-        day = "Weekday";
-    }
+
     res.render("list", {kindOfDay: day});
 })
 
