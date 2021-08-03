5196e7e5d598af7aef157979cab63c59554fa4b6
Sajal Gupta <guptasajal411@gmail.com>
2021-08-03 15:16:44 +0530

    Add work list path for new list

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { response } = require('express');
 var items = ["Buy food", "Cook food", "Eat food"];
+var workItems = [];
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,7 +11,7 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     var today = new Date();
     var options = {
         weekday: "long",
@@ -19,16 +20,33 @@ app.get("/", function(req, res){
     };
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", { listTitle: day, newListItems: items });
 })
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     console.log(req.body.newItem);
     var item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.get("/work", function (req, res) {
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+})
+
+app.post("/work", function (req, res) {
+    let item = req.body.item;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.listen(3000, function () {
     console.log("Server listening to port 3000");
 })
