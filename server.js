60b2f95cb3aab97c527f672736df905336ec9d24
Sajal Gupta <guptasajal411@gmail.com>
2021-08-24 12:54:00 +0530

    Add options for deleting tasks from MongoDB database

--- server.js
+++ server.js
@@ -59,6 +59,18 @@ app.post("/", function (req, res) {
     res.redirect("/");
 })
 
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndDelete(checkedItemId, function (err){
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Item with ID: " + checkedItemId + " was successfully removed.");
+        }
+    });
+    res.redirect("/");
+})
+
 app.get("/work", function (req, res) {
     res.render("list", { listTitle: "Work List", newListItems: workItems });
 })
